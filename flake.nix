3822612b93063c5cf6eada0beb39d4cf50efe485
Alex Rosegrid <rosegrid58@gmail.com>
2026-07-11 19:57:02 +0530

    chore: Add nix build command

--- flake.nix
+++ flake.nix
@@ -1,15 +1,43 @@
 {
-  description = "Devshell Declaration";
+  description = "Derivation for CHIP-8 Emulator";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs, systems, ... }: let
+    forEachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f {
+      pkgs = import nixpkgs { inherit system; };
+    });
   in {
-    devShells.${system} = {
+    packages = forEachSystem ({ pkgs, ... }: {
+      default = pkgs.stdenv.mkDerivation {
+        pname = "chip8-emu";
+        version = "0.1.0";
+        src = ./.;
+        nativeBuildInputs = with pkgs; [
+          cmake
+        ];
+        buildInputs = with pkgs; [
+          sdl2-compat
+          nlohmann_json
+        ];
+        installPhase = ''
+          mkdir -p $out/bin
+          cp dummy.out $out/bin/chip8-emu
+        '';
+      };
+    });
+
+    apps = forEachSystem ({ pkgs, ... }: {
+      default = {
+        type = "app";
+        program = "${self.packages.${pkgs.system}.default}/bin/chip8-emu";
+      };
+    });
+
+    devShells = forEachSystem ({ pkgs, ... }: {
       default = pkgs.mkShell {
         packages = with pkgs; [
           sdl2-compat
@@ -18,6 +46,6 @@
           cmake
         ];
       };
-    };
+    });
   };
 }
