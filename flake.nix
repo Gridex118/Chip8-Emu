38912c568f7b4c2b9e6a6e7b01530800fb39a44d
Alex Rosegrid <rosegrid58@gmail.com>
2026-07-11 13:54:01 +0530

    chore: Add a flake.nix file

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Devshell Declaration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          sdl2-compat
+          nlohmann_json
+          pkg-config
+          cmake
+        ];
+      };
+    };
+  };
+}
